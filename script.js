2c833aa065be6d8c979c9f08779a70daed4b4ce1
Matei Popescu <82405419+mtlvsdn@users.noreply.github.com>
2021-12-11 18:42:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+/*var voice1 = new Audio("voice1.m4a");
+document.onclick = function() {
+    voice1.play();
+}*/
+
+/*var voice1 = document.getElementById("voice1");
+
+function playVoice1() {
+    voice1.play();
+}*/
+
+/*const keys = document.querySelectorAll('.sunet');
+
+keys.forEach((key) => {
+    key.addEventListener('click', () => playNote(key));
+});
+
+let playNote = (key) => {
+    const noteSound = document.getElementById(key.dataset.note);
+    noteSound.play();
+};*/
